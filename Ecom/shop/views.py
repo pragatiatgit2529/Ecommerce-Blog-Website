b310d1b6a2f4ba3fd7ed0d5c24f832b23dce82fc
yatharth25 <54071856+yatharth25@users.noreply.github.com>
2020-07-20 18:34:44 +0530

    added logic and category wise slides

--- Ecom/shop/views.py
+++ Ecom/shop/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
+from .models import Product
+from math import ceil
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'shop/index.html')
+    products = Product.objects.all()
+    print(products)
+    allprods=[]
+    catprods= Product.objects.values('category', 'id')
+    cats= {item['category'] for item in catprods}
+    for cat in cats:
+        prod=Product.objects.filter(category=cat)
+        n = len(prod)
+        nslides = n // 4 + ceil((n / 4) - (n // 4))
+        allprods.append([prod, range(1,nslides), nslides])
+    params = {'allprods':allprods}
+    return render(request, 'shop/index.html',params)
 
 def about(request):
-    return HttpResponse("Hello shop")
+    return render(request, 'shop/about.html')
 
 def contact(request):
     return HttpResponse("Hello shop")
